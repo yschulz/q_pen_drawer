92de69bae25d16fccefac51d7f8e6bd97189a220
Yannick Schulz <yschulz854@gmail.com>
2024-04-01 15:42:55 +0200

    adds basic structure for a qt executable that builds with colcon

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "q_pen_drawer/mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
